b6e7a18fb4ead266c5ff1b4a0c68221dfd60152f
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-27 20:22:39 -0300

    teste

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 int main(){
 
     double c = 4.69873456164;
+    double d = 9.0908;
 
     std::cout<<8+2<<std::endl;
     std::cout<<10-4<<std::endl;
     std::cout<<4*3<<std::endl;
     std::cout<<8/2<<std::endl;
+    std::cout<<d<<std::endl;
+    
 
     return 0;
 }
