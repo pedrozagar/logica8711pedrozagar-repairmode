45e213becd05deb03f2ca67079a7987f017d68ea
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-27 19:48:46 -0300

    novocodigo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string saudacao = "goodnight, mr. Pedro!";
+    int a = 8;
+    //int numero inteiro
+    float b = 9.6;
+    //float numero de ponto flutuante ocupa 4bytes
+    double c = 4.6;
+    //double ocupa 8bytes
 
-    std::cout<<saudacao<<std::endl;
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
 
-    return 0;
 }
