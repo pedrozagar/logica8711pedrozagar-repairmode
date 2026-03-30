41aa655a1e6553d0fea56d79e20720d8f81ecac5
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-30 20:29:36 -0300

    comparação de valores com a linguagem de maquina

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string nome;
-    int idade;
-    float altura;
+    int a;
+    int b;
+    //int a, b;
 
-    std::cout<<"olá ser humano!" <<std::endl;
+    std::cout<<"digite um valor para A: "<<std::endl;
+    std::cin>>a;
 
-    std::cout<<"qual seu nome: " <<std::endl;
-    std::cin>>nome;
-    std::cout<<"qual sua idade: " <<std::endl;
-    std::cin>>idade;
-    std::cout<<"qual sua altura: " <<std::endl;
-    std::cin>>altura;
- 
-    std::cout<<"bem-vindo, senhor "<<nome<<std::endl;
-    std::cout<<idade<< " anos." <<std::endl;   
-    std::cout<<altura<< "m." <<std::endl;
+    std::cout<<"digite um valor para B: "<<std::endl;
+    std::cin>>b;
 
+    std::cout<<(a > b)<<std::endl;
+    //aqui a pergunta é a é maior que b? se for a maior que b sera true, ou seja, 1, que é linguagem de maquina. se a é maior que b, daí vai ser falso, será 0, ou seja, a linguagem de maquina especifica por binario 0 e 1;
+
+    std::cout<<(a >= b)<<std::endl; 
+    // a é igual ou maior que b?;
+    std::cout<<(a != b)<<std::endl;
+    // a é se o valor é diferente;
      return 0;
 }
