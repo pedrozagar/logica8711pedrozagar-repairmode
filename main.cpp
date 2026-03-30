b34f8deba19d788752862194c19b84f5ea62cb7f
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-30 20:41:11 -0300

    comparação 2

--- main.cpp
+++ main.cpp
@@ -12,12 +12,21 @@ int main(){
     std::cout<<"digite um valor para B: "<<std::endl;
     std::cin>>b;
 
+    std::cout<<"a e maior que b: "<<std::endl;
     std::cout<<(a > b)<<std::endl;
     //aqui a pergunta é a é maior que b? se for a maior que b sera true, ou seja, 1, que é linguagem de maquina. se a é maior que b, daí vai ser falso, será 0, ou seja, a linguagem de maquina especifica por binario 0 e 1;
 
+    std::cout<<"a e igual ou maior que b: "<<std::endl;
     std::cout<<(a >= b)<<std::endl; 
     // a é igual ou maior que b?;
+
+    std::cout<<"a e diferente que b: "<<std::endl;
     std::cout<<(a != b)<<std::endl;
     // a é se o valor é diferente;
+
+    std::cout<<"a e igual ou igual a b: "<<std::endl;
+    std::cout<<(a == b)<<std::endl;
+    // um é atribuição e outro é comparação
+
      return 0;
 }
