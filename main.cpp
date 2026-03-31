cdfa4e6748a90200f3542b95a5b2bb6a0b418a62
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-30 21:15:27 -0300

    somas e somas

--- main.cpp
+++ main.cpp
@@ -2,31 +2,12 @@
 
 int main(){
 
-    int a;
-    int b;
-    //int a, b;
+    int soma1 = 100 + 50;
+    int soma2 = soma1 + 50;
+    int soma3 = soma2 + 100;
 
-    std::cout<<"digite um valor para A: "<<std::endl;
-    std::cin>>a;
-
-    std::cout<<"digite um valor para B: "<<std::endl;
-    std::cin>>b;
-
-    std::cout<<"a e maior que b: "<<std::endl;
-    std::cout<<(a > b)<<std::endl;
-    //aqui a pergunta é a é maior que b? se for a maior que b sera true, ou seja, 1, que é linguagem de maquina. se a é maior que b, daí vai ser falso, será 0, ou seja, a linguagem de maquina especifica por binario 0 e 1;
-
-    std::cout<<"a e igual ou maior que b: "<<std::endl;
-    std::cout<<(a >= b)<<std::endl; 
-    // a é igual ou maior que b?;
-
-    std::cout<<"a e diferente que b: "<<std::endl;
-    std::cout<<(a != b)<<std::endl;
-    // a é se o valor é diferente;
-
-    std::cout<<"a e igual ou igual a b: "<<std::endl;
-    std::cout<<(a == b)<<std::endl;
-    // um é atribuição e outro é comparação
+    std::cout<<soma2<<std::endl;
+    std::cout<<soma3<<std::endl;
 
      return 0;
 }
