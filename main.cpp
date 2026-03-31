edae95e5a47a23449b166e2d3595514413a68d43
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-30 21:40:05 -0300

    incrementação

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 int main(){
 
     int a = 10;
-    int b = 3;
-
-    std::cout<<(a % b)<<std::endl;
-    std::cout<<(a / b)<<std::endl;
+    
+    std::cout<<++a<<std::endl;
+    //o ++a significa incrementar, ou seja, adicionar mais 1 ao int a que é igual a 10, que daí vai ficar 11;
+    std::cout<<++a<<std::endl;
+    std::cout<<++a<<std::endl;
 
      return 0;
 }
