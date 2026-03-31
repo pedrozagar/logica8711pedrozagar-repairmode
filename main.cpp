d291694784d14fe3bc6eb9ae0e92003d5d597fb1
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-03-30 21:51:02 -0300

    a = a + 5

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 int main(){
 
     int a = 10;
-    
-    std::cout<<++a<<std::endl;
-    //o ++a significa incrementar, ou seja, adicionar mais 1 ao int a que é igual a 10, que daí vai ficar 11;
-    std::cout<<++a<<std::endl;
-    std::cout<<++a<<std::endl;
+
+    a += 5;
+    //a = a + 5
+
+    std::cout<<a<<std::endl;
+
 
      return 0;
 }
