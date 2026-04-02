d2733b9f88f2bacf7f33bd845b31ab28e9b9caef
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-01 21:33:12 -0300

    comentarios sobre endline - espaço entre linhas no terminal

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 
 int main(){
 
-    int num1, num2, soma;
+    int num1;
 
-    std::cout<<"Digite num 1: "<<"\n";
+    std::cout<<"Digite num 1: "<<std::endl;
     std::cin>>num1;
 
-    std::cout<<"Digite num 2: "<<"\n";
-    std::cin>>num2; 
+    std::cout<<"\n\nO num 1 é "<<num1<<"\n";
+    // std::cout<<"\n\n\n;"
 
-    soma = num1 + num2;
+    // New Line: Apenas insere o caractere de nova linha.
+    // É como apertar o enter em um editor de texto. 
+    // O computador guarda essa informação em um espaço temporário (chamado buffer) e só mostra
+    // na tela quando esse espaço enche ou o programa acaba.
 
-    std::cout<<"O resultado da soma é "<<soma<<"\n";
+    // std::endl Insere a nova linha E foraça o esvaziamento do buffer (operação chamada de flush). 
+    // Ele obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela.
     
      return 0;
 }
