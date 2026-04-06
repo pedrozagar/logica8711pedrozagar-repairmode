194e88bf491339b79d39fe405e8c0078da1b377b
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-06 20:32:14 -0300

    append

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
 #include<iostream>
+#include<string>
 
 int main(){
+     
+    std::string nome;
+    std::string sobrenome;
 
-    int num1;
+    std::cout<<"Digite seu nome: "<<std::endl;
+    std::cin>>nome;
 
-    std::cout<<"Digite num 1: "<<std::endl;
-    std::cin>>num1;
+    std::cout<<"Digite seu sobrenome: "<<std::endl;
+    std::cin>>sobrenome;
 
-    std::cout<<"\n\nO num 1 é "<<num1<<"\n";
-    // std::cout<<"\n\n\n;"
+    std::cout<<nome.append(" ").append(sobrenome);
 
-    // New Line: Apenas insere o caractere de nova linha.
-    // É como apertar o enter em um editor de texto. 
-    // O computador guarda essa informação em um espaço temporário (chamado buffer) e só mostra
-    // na tela quando esse espaço enche ou o programa acaba.
-
-    // std::endl Insere a nova linha E foraça o esvaziamento do buffer (operação chamada de flush). 
-    // Ele obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela.
-    
      return 0;
 }
