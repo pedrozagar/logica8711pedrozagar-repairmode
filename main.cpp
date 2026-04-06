5f94bdc4e49df10962f67657979d8bd2af618c65
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-06 20:45:30 -0300

    int + string 2

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
 int main(){
      
     int a = 40;
-    int b = 10;
-    std::string c = "40";
     std::string d = "10";
 
-    std::cout<<a+b<<std::endl;
-    std::cout<<c+d<<std::endl;
-
+    std::cout<<a+d<<std::endl;
+    
      return 0;
 }
