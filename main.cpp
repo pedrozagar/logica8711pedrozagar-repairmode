ea604f7331d918b39586b88fea926a357c764fa0
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-06 21:54:46 -0300

    inicio no 0

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 #include<string>
-#include<windows.h>
 
 int main(){
      
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
+    std::string nome = "Pedro";
 
-    int pedro[5] = {10,20,30,40,50};
+    std::cout<<nome[0]<<std::endl;
+    std::cout<<nome[2]<<std::endl;
+    std::cout<<nome[nome.length()-1]<<std::endl;
+    nome[0] = 'A';
+    std::cout<<nome<<std::endl;
 
-    std::cout<<pedro[1];
-           
+
     return 0;
 }
