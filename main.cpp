731a269d48efc479ec226846c498a3e28888189a
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-13 19:40:23 -0300

    if e else idades

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ int main(){
 
     int idade;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+        std::cout<<"Digite a sua idade: "<<std::endl;
+        std::cin>>idade;
 
-    if(idade <= 0){
-    std::cout<<"Idade invalida!"<<std::endl;
+    if(idade < 0){
+        std::cout<<"Idade invalida!"<<std::endl;
+    }else if(idade <= 2){
+        std::cout<<"Voce e um bebe!"<<std::endl;
     }
 
     return 0;
