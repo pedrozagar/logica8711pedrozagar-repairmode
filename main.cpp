da63b455cd4c5303f51fc9396fffd3c9a8489f2c
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-15 19:20:20 -0300

    for imprimindo 10x algo escrito na saída

--- main.cpp
+++ main.cpp
@@ -3,30 +3,10 @@
 
 int main(){
 
-    std::string nome;
-    float altura;
-    int idade;
-
-    std::cout<<"ola, bem-vindo.\n"<<std::endl;
-
-    std::cout<<"Digite seu nome: "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Digite sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Digite sua idade: "<<std::endl;
-    std::cin>>idade;
-
-    if (idade < 0){
-        std::cout<<"Idade invalida!"<<std::endl;
-    }else if(idade <=10){
-        std::cout<<"Voce e infantil."<<std::endl;
-    }else if(idade <=18){
-        std::cout<<"Voce e adolescente."<<std::endl;
-    }else if(idade <=60){
-        std::cout<<"Voce e adulto."<<std::endl;
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"Este e um loop!!"<<std::endl;
     }
+ 
 
     return 0;
 }
