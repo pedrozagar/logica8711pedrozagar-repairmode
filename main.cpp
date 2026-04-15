199ac6fca648dff7fe4950a169d2ba556bc9436d
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-15 19:50:50 -0300

    for - cronometragem de segundos

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> //para definir unidades de tempo (ms, segundos)
 
 int main(){
 
     for(int i = 10; i >= 0; i--){
         std::cout<<"A bomba ira explodir em..."<<i<<std::endl;
         
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        //seconds -> milliseconds
     }
-        std::cout<<"\n"<<std::endl;
-        std::cout<<"Parabens, voce e um genio.\n"<<std::endl;
+        std::cout<<"\n";
+        std::cout<<"perfeito."<<std::endl;
 
     return 0;
 }
