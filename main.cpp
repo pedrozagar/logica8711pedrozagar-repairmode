3334fc4d217c0535b1da75f3b1b29eae7d60b0c5
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-15 21:57:51 -0300

    myNumbers i++ ++i

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 int main(){
 
-    int myNumbers[5] = {10, 20, 30, 40, 50};
+    int myNumbers[9] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
     //for(int num: myNumbers){
     //      std::cout<<num<<std::endl;
    
-    for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < 9; i++){
         std::cout<<myNumbers[i]<<std::endl;
     }
     
-    
+
     return 0;
 }
