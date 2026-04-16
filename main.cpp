7c9535b3660b2aa49ebd7662a8d7f21109351b12
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-15 21:42:29 -0300

    leitura do array dos numeros que possuo direto na tela

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 int main(){
 
     int myNumbers[5] = {10, 20, 30, 40, 50};
-    for(int num : myNumbers){
-        std::cout<<num<<std::endl;
+    //for(int num: myNumbers){
+    //      std::cout<<num<<std::endl;
+   
+    for(int i = 0; i < 5; ++i){
+        std::cout<<myNumbers[i]<<std::endl;
     }
-
+    
+    
     return 0;
 }
