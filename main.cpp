62063b01317d8395edd31e47551d369e5c288201
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-15 21:11:34 -0300

    soma no for int

--- main.cpp
+++ main.cpp
@@ -2,36 +2,13 @@
 
 int main(){
 
-    int opcao;
+    int soma = 0;
 
-    std::cout<<"Digite um valor para opcao: "<<std::endl;
-    std::cin>>opcao;
-
-    switch(opcao){
-        case 1:
-            std::cout<<"Domingo."<<std::endl;
-            break;
-        case 2:
-            std::cout<<"Segunda-feira."<<std::endl;
-            break;
-        case 3:
-            std::cout<<"Terca-feira."<<std::endl;
-            break;
-        case 4:
-            std::cout<<"Quarta-feira."<<std::endl;
-            break;
-        case 5:
-            std::cout<<"Quinta-feira."<<std::endl;
-            break;
-        case 6:
-            std::cout<<"Sexta-feira."<<std::endl;
-            break;
-        case 7:
-            std::cout<<"Sabado."<<std::endl;
-            break;
-        default:
-            std::cout<<"Numero invalido!"<<std::endl;
+    for(int i = 1; i <= 5; i++){
+        soma = soma + i;
     }
+
+    std::cout<<"Soma e: "<<soma<<std::endl;
 
     return 0;
 }
