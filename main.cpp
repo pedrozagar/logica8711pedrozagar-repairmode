bcd46a348a71df48ee4a54032de16845a3a0e20e
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-28 20:01:58 -0300

    exercicio foreach com anotações

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ int main(){
 
     int myNumbers [5] = {1, 2, 3, 4, 5};
 
-    for(int i = 0; i < 5; i++){
-        std::cout<<myNumbers[i]<<std::endl;
+    //for(int i = 0; i < 5; i++){
+    //    std::cout<<myNumbers[i]<<std::endl;
+    //}
+
+    for(int num : myNumbers){
+        std::cout<<num<<std::endl;
     }
       
     return 0;
