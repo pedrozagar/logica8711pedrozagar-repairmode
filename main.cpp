7313758ddddabaa79226fb58d88f4928845b1d6a
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-28 20:14:46 -0300

    introducao ao void

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 
-int main(){
-
-    int myNumbers [5] = {1, 2, 3, 4, 5};
-
-    //for(int i = 0; i < 5; i++){
-    //    std::cout<<myNumbers[i]<<std::endl;
-    //}
-
-    for(int num : myNumbers){
-        std::cout<<num<<std::endl;
+    void myFunction(){
+        std::cout<<"Olá mundo!"<<std::endl;
     }
       
+int main(){
+
+        myFunction(); //aqui é chamar opção.
+
     return 0;
 
 }
