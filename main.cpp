4285e119e0e2954ddd0d3d93f6e6ea0769c11cf0
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-28 21:12:33 -0300

    void e string 2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 #include<string>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+    std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 int main(){
 
-    minhaFuncao("Pedro");
-    minhaFuncao("Nome");
+    minhaFuncao("Pedro", 28);
 
     return 0;
 
