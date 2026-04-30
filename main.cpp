d9272918128677395e4199a29d364afd6de4b9f8
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-04-30 19:37:40 -0300

    exercicio de if (se sim) e else (se nao)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
-
-int doubleGame(int x){
-    return x * 2;
-}
+#include<string>
 
 int main(){
 
-    for(int i = 1; i <= 5; i++){
-        std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
-    }  
+    int opcao;
+    std::string nome;
+    int idade;
+
+    std::cout<<"Escolha a opcao adequada: "<<std::endl<<std::endl;
+    std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
+    std::cin>>opcao;
+
+if(opcao == 1){
+    std::cout<<"Digite o nome: "<<std::endl;
+    std::cin>>nome;
+    std::cout<<"Digite a idade: "<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"Seu nome e: "<<nome<<std::endl;
+    std::cout<<"Sua idade e: "<<idade<<std::endl;
+    std::cout<<"Certo, este e seria o if quando voce apertou o 1, parabens."<<std::endl;
+}else{
+    std::cout<<"Ate a proxima!"<<std::endl;
+}
 
     return 0;
 
