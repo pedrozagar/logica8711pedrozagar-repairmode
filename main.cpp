640452b9d109796802b15985cfe99488c4691d38
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-12 19:23:32 -0300

    calculadora simples

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string nomeCompleto;
+    int num1, num2, soma, subt, mult;
+    float div;
 
-    std::cout<<"Digite o nome completo do aluno: "<<std::endl;
-    std::getline(std::cin, nomeCompleto);
+    std::cout<<"Digite aqui o valor para numero 1: "<<std::endl;
+    std::cin>>num1;
 
-    std::cout<<nomeCompleto<<std::endl;
-    
+    std::cout<<"Digite aqui o valor para numero 2: "<<std::endl;
+    std::cin>>num2;
+
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+
+    std::cout<<"O resultado da soma e: "<<soma<<std::endl;
+    std::cout<<"O resultada da subtracao e: "<<subt<<std::endl;
+    std::cout<<"O resultado da multiplicacao e: "<<mult<<std::endl;
+    std::cout<<"O resultado da divisao e: "<<div<<std::endl;
+
     return 0;
-
 }
