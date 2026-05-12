a4568121cb766f6e520b4ad123134d56651f3ee9
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-12 20:54:17 -0300

    mais calculadora

--- main.cpp
+++ main.cpp
@@ -13,7 +13,12 @@ float multiplicar(float a, float b){
 }
 
 float dividir(float a, float b){
-    return a / b;
+    if(b == 0){
+        std::cout<<"Nao e possivel dividir por 0!"<<std::endl;
+        return 0;
+    }else{
+        return a / b;
+    }
 }
 
 void coletarNumeros(float &a, float &b){
