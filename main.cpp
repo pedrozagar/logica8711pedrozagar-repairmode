be75f76eddec411e8cf76c3face761556b84d75a
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-26 20:09:15 -0300

    função cubo n * n * n = n³

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include<iostream> //biblioteca
 
 int quadrado(int n){
-    return n * n;
+    return n * n; //n² é ao quadrado
 }
+
+int cubo(int n){
+        return n * n * n; //n³ é ao cubo
+    }
 
 int main(){
 
     std::cout<<"Quadrado de 5: "<<quadrado(5)<<std::endl;
     std::cout<<"Quadrado de 8: "<<quadrado(8)<<std::endl;
     std::cout<<"Quadrado de 10: "<<quadrado(10)<<std::endl;
+
+    std::cout<<"\n";
+
+    std::cout<<"Cubo de 5: "<<cubo(5)<<std::endl;
+    std::cout<<"Cubo de 8: "<<cubo(8)<<std::endl;
+    std::cout<<"Cubo de 10: "<<cubo(10)<<std::endl;
 
     return 0;
 }
