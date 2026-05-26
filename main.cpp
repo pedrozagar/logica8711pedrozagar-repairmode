7c2a5f371d92fa97183811ebd8a88b4c424a0fd4
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-26 19:53:41 -0300

    exemplo fatorial de calculo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
-#include<iostream> //biblioteca aqui
-#include<format> //biblioteca aqui
-#include<string> //biblioteca aqui
+#include<iostream> //biblioteca
+#include<string> //biblioteca
+
+//long long é uma nova função que iremos aprender
+
+long long calcularFatorial(int n){
+    long long resultado = 1;
+
+    for(int i = 1; i <= n; i++){
+        resultado *= i;
+    }
+
+    return resultado;
+}
 
 int main(){
+    int numero;
 
-    std::string nome = "Pedro";
+    std::cout<<"Digite um numero para calcular fatorial: "<<std::endl; //aqui é importante entender o que significa FATORIAL.
+    std::cin>>numero;
 
-    std::string mensagem = std::format("Ola, {}! Voce tem {} anos.\n", nome);
-    std::cout<<mensagem;
-
-    return 0;
+    if(numero < 0){
+        std::cout<<"Erro: fatorial de numero negativo nao existe!"<<std::endl;
+    }else{
+        long long fatorial = calcularFatorial(numero);
+        std::cout<<"! = "<<fatorial<<std::endl;
+    }
 }
