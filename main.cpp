ed03d2d896ca71ebea2400bf128d9b54cce52672
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-26 20:01:15 -0300

    função quadrado

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
 #include<iostream> //biblioteca
-#include<string> //biblioteca
 
-//long long é uma nova função que iremos aprender
-
-long long calcularFatorial(int n){
-    long long resultado = 1;
-
-    for(int i = 1; i <= n; i++){
-        resultado *= i;
-    }
-
-    return resultado;
+int quadrado(int n){
+    return n * n;
 }
 
 int main(){
-    int numero;
 
-    std::cout<<"Digite um numero para calcular fatorial: "<<std::endl; //aqui é importante entender o que significa FATORIAL.
-    std::cin>>numero;
+    std::cout<<"Quadrado de 5: "<<quadrado(5)<<std::endl;
+    std::cout<<"Quadrado de 8: "<<quadrado(8)<<std::endl;
+    std::cout<<"Quadrado de 10: "<<quadrado(10)<<std::endl;
 
-    if(numero < 0){
-        std::cout<<"Erro: fatorial de numero negativo nao existe!"<<std::endl;
-    }else{
-        long long fatorial = calcularFatorial(numero);
-        std::cout<<"! = "<<fatorial<<std::endl;
-    }
+    return 0;
 }
