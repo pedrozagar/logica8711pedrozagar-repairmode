7b9f0cc3ec2651c5ab3e61671f79affa01580195
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-26 21:43:58 -0300

    numeros primos e não primos

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include<iostream>
 
-int encontrarMaior(int arr[], int tamanho){
-    int maior = arr[0];
+bool ehPrimo(int numero){
+    if(numero <= 1){
+        return false;
+    }
 
-    for(int i = 1; i < tamanho; i++){
-        if(arr[i] > maior){
-            maior = arr[i];
+    for(int i = 2; i < numero; i++){
+        if(numero % i == 0){
+            return false;
         }
     }
 
-    return maior;
+    return true;
 }
 
 int main(){
-    int numeros[5];
+    int numero;
 
-    std::cout<<"Digite 5 numeros: "<<std::endl;
-        for(int i = 0; i < 5; i++){
-            std::cout<<"Digite o numero "<<(i + 1)<<": ";
-            std::cin>>numeros[i];
-        }
+    std::cout<<"Digite um numero: "<<std::endl;
+    std::cin>>numero;
 
-        std::cout<<"Maior numero: "<<encontrarMaior(numeros, 5)<<std::endl;
+    if(ehPrimo(numero)){
+        std::cout<<numero<<" e primo!"<<std::endl;
+    }else{
+        std::cout<<numero<<" nao e primo!"<<std::endl;
+    }
 
     return 0;
 }
