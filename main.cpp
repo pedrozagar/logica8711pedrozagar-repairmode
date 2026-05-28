87cb17a74529aafeabea2a524f0c41c1f33a0d53
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-28 19:40:06 -0300

    exemplo de fibonacci

--- main.cpp
+++ main.cpp
@@ -1,30 +1,19 @@
 #include<iostream>
-#include<tuple>
-#include<string>
-
 
 int main(){
 
-    std::string nome;
-    int idade;
-    double altura;
+    int fibonacci[10];
 
-    std::cout<<"Digite o seu nome: "<<std::endl;
-    std::getline(std::cin, nome);
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+    for(int i = 2; i < 10; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
 
-    std::cout<<"Digite a sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
-
-    std::cout<<"\n ----- Dados salvos na TUPLA -----"<<std::endl;
-    
-    std::cout<<"Nome: "<<std::get<0>(pessoa)<<std::endl;
-    std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
-    std::cout<<"Altura: "<<std::get<2>(pessoa)<<"m"<<std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout<<fibonacci[i]<<"\n";
+    }
 
     return 0;
 }
