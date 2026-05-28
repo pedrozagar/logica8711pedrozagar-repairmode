2a18573343df9ffda5a43c1c143c61089e70ff1a
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-05-28 20:52:39 -0300

    Adiciona fibonacci recursivo com logs de execução

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 
 int fibonacci(int n){
+    std::cout<<"Calculando fib("<<n<<")"<<std::endl;
 
-    if(n == 0) return 0;
-    if(n == 1) return 1;
-
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if(n == 0){
+        std::cout<<"Fib(0) = 0 (PARADA)"<<std::endl;
+        return 1;
+    }
+    if(n == 1){
+        std::cout<<"Fib(1) = 1 (PARADA!)"<<std::endl;
+        return 1;
+    }
+    
+    std::cout<<"Fib("<<n<<") = Fib("<<(n-1)<<") + fib("<<(n-2)<<")\n";
+    int resultado = fibonacci(n - 1) + fibonacci(n - 2);
+    std::cout<<"Fib(" << n << ") = "<<resultado<<"\n";
+    return resultado;
 }
 
 int main(){
 
-    std::cout<<fibonacci(6)<<std::endl;
+    fibonacci(4);
+
     return 0;
+
 }
