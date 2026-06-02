75ff5462dc3c0b7304b3d724659e7facc3123013
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-02 19:37:41 -0300

    palíndrono

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include<iostream>
+#include<string>
+#include<cctype>
 
-int somarVetor(int vetor[], int indice){
-    if(indice == 0){
-        return vetor[0];
+bool ehPalindromo(std::string s){
+    int inicio = 0;
+    int fim = s.length() - 1;
+    while(inicio < fim){
+        if(s[inicio] != s[fim]){
+            return false;
+        }
+        inicio++;
+        fim--;
     }
-    return vetor[indice] + somarVetor(vetor, indice -1);
+    return true;
 }
 
-int main(){
+int main (){
 
-    int numeros[5] = {10, 20, 30, 40, 50};
+    std::string palavra;
 
-    std::cout<<"Soma recursiva: "<<somarVetor(numeros, 4)<<std::endl;
+    std::cout<<"Digite uma palavra: "<<std::endl;
+    std::cin>>palavra;
 
+    for(int i = 0; i < palavra.length(); i++){
+        palavra[i] = std::tolower(palavra[i]);
+    }
+    if(ehPalindromo(palavra)){
+        std::cout<<"E palindrono!"<<std::endl;
+    }else{
+        std::cout<<"Nao e palindrono!"<<std::endl;
+    }
     return 0;
 }
