17c5ddd8831068f4f8ba7896bc47cd3e99d082b5
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-02 21:16:56 -0300

    exemplo de struct - chega próximo de orientação voltada a objetos

--- main.cpp
+++ main.cpp
@@ -7,17 +7,13 @@ struct Pessoa{
     float altura;
 };
 
-int main(){
+int main(){   
 
-    Pessoa p1;
-
-    p1.nome = "pedro";
-    p1.idade = 28;
-    p1.altura = 1.79;
-
-    std::cout<<"nome: "<<p1.nome<<std::endl;
-    std::cout<<"idade: "<<p1.idade<<std::endl;
-    std::cout<<"altura: "<<p1.altura<<std::endl;
+    Pessoa p1 = {"pedro", 20, 1.80};
+    Pessoa p2 = {"junior", 30, 1.70};
+    
+    std::cout<<p1.nome<<" tem "<<p1.idade<<" anos "
+    <<"e mede "<<p1.altura<<"m"<<std::endl;
 
     return 0;
 }
