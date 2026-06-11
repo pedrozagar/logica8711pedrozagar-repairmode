93f483a5ddac590ecb397adda6958f330df4d153
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-11 20:58:14 -0300

    maior elemento

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 
-int mdc(int a, int b){
-    if(b == 0) return a;
-    return mdc(b, a % b);
+int encontrarMaior(int* arr, int tamanho){
+    int maior = arr[0];
+    for(int i = 1; i < tamanho; i++){
+        if(arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return maior;
 }
 
 int main(){
-    std::cout<<"mdc(48, 18) = "<<mdc(48, 18)<<std::endl;
-    std::cout<<"mdc(100, 50) = "<<mdc(100, 50)<<std::endl;
-
+    int arr[] = {3,7,2,9,1,5};
+    std::cout<<"maior elemento: "<<encontrarMaior(arr, 6)<<std::endl;
     return 0;
 }
