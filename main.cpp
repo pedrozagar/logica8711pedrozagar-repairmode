c762d048630a018389d4105074f4d9ec8ab773bc
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-11 19:55:59 -0300

    quickSort

--- main.cpp
+++ main.cpp
@@ -1,24 +1,34 @@
 #include<iostream>
-#include<string>
- 
-    struct Aluno{
-        std::string nome;
-        float nota;
-    };
- 
- 
-int main(){
- 
-    Aluno alunos[3] = {
-        {"herique", 8.5},
-        {"Paula", 9.0},
-        {"Juliana", 7.0}
-    };
- 
-    for(int i = 0; i < 3; i++){
-        std::cout<<alunos[i].nome<<": "<<alunos[i].nota<<std::endl;
+
+void quickSort(int* arr, int esquerda, int direita){
+    if(esquerda >= direita) return;
+
+    int pivo = arr[direita];
+    int i = esquerda - 1;
+
+    for(int j = esquerda; j < direita; j++){
+        if(arr[j] < pivo){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
-   
-   
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[direita];
+    arr[direita] = temp;
+
+    quickSort(arr, esquerda, i);
+    quickSort(arr, i + 2, direita);
+}
+int main(){
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int tamanho = 7;
+
+    quickSort(arr, 0, tamanho - 1);
+
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<arr[i]<<" ";
+    }
     return 0;
 }
