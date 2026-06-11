d8a670ee2620f57764653632c6347d3709bfda82
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-11 20:24:58 -0300

    mdc

--- main.cpp
+++ main.cpp
@@ -1,30 +1,13 @@
 #include<iostream>
 
-void bubbleSort(int* arr, int tamanho){
-    for(int i=0; i<tamanho-1; i++){
-        for(int j=0; j<tamanho-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+int mdc(int a, int b){
+    if(b == 0) return a;
+    return mdc(b, a % b);
 }
-int main(){
-    int arr[] = {64,34,25,12,22,11,90};
-    int tamanho = std::size(arr);
-    std::cout<<"array original: "<<std::endl;
-    for(int i = 0; i < tamanho; i++){
-        std::cout<<arr[i]<<" ";
-    }
-    std::cout<<"\n";
 
-    bubbleSort(arr, tamanho);
-    std::cout<<"array ordenado: ";
-    for(int i = 0; i < tamanho; i++){
-        std::cout<<arr[i]<<" ";
-    }
-    std::cout<<"\n";
+int main(){
+    std::cout<<"mdc(48, 18) = "<<mdc(48, 18)<<std::endl;
+    std::cout<<"mdc(100, 50) = "<<mdc(100, 50)<<std::endl;
+
     return 0;
 }
