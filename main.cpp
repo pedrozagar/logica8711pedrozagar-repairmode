5cd30bfcec3b81858678569ddf9d0c43d8fceb9b
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-11 20:18:42 -0300

    bubbleSort

--- main.cpp
+++ main.cpp
@@ -1,34 +1,30 @@
 #include<iostream>
 
-void quickSort(int* arr, int esquerda, int direita){
-    if(esquerda >= direita) return;
-
-    int pivo = arr[direita];
-    int i = esquerda - 1;
-
-    for(int j = esquerda; j < direita; j++){
-        if(arr[j] < pivo){
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+void bubbleSort(int* arr, int tamanho){
+    for(int i=0; i<tamanho-1; i++){
+        for(int j=0; j<tamanho-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[direita];
-    arr[direita] = temp;
-
-    quickSort(arr, esquerda, i);
-    quickSort(arr, i + 2, direita);
 }
 int main(){
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int tamanho = 7;
-
-    quickSort(arr, 0, tamanho - 1);
-
+    int arr[] = {64,34,25,12,22,11,90};
+    int tamanho = std::size(arr);
+    std::cout<<"array original: "<<std::endl;
     for(int i = 0; i < tamanho; i++){
         std::cout<<arr[i]<<" ";
     }
+    std::cout<<"\n";
+
+    bubbleSort(arr, tamanho);
+    std::cout<<"array ordenado: ";
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
     return 0;
 }
