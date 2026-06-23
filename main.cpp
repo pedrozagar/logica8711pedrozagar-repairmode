2d02572bcd6d4f5a1c6f8ce6713dd078993630dc
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-23 20:26:16 -0300

    + exercícios de Array

--- main.cpp
+++ main.cpp
@@ -1,69 +1,26 @@
 #include<iostream>
 
 int main(){
-    int fila[10];
-    int inicio = 0;
-    int fim = 0;
-    int opcao;
 
-    std::cout<<"============FILA COM MENU============"<<std::endl;
-    
+    int numeros[] = {1, 2, 3, 2, 4, 5, 3, 6};
+    int tamanho = 8;
+
+    std::cout<<"======== NUMEROS DUPLICADOS ========"<<std::endl;
     std::cout<<std::endl;
 
-    while(true){
-        std::cout<<"1. enfileirar"<<std::endl;
-        std::cout<<"2. desenfileirar"<<std::endl;
-        std::cout<<"3. exibir fila"<<std::endl;
-        std::cout<<"4. sair"<<std::endl;
+    std::cout<<"array: ";
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<numeros[i]<<" ";
+    }
+    std::cout<<std::endl;
 
-        std::cout<<std::endl;
-
-        std::cout<<"escolha: ";
-        std::cin>>opcao;
-
-        std::cout<<std::endl;
-
-        if(opcao == 1){
-            if(fim < 10){
-                int valor;
-                std::cout<<"digite o valor: ";
-                std::cin>>valor;
-                fila[fim] = valor;    
-                fim++;
-
-                std::cout<<std::endl;
-
-                std::cout<<"enfileirado!"<<std::endl;
-                std::cout<<std::endl;
-            }else{
-                std::cout<<"fila cheia!"<<std::endl;
+    std::cout<<"numeros duplicados: "<<std::endl;
+    for(int i = 0; i < tamanho; i++){
+        for(int j = i + 1; j < tamanho; j++){
+            if(numeros[i] == numeros[j]){
+                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
+                break;
             }
-        }
-        else if(opcao == 2){
-            if(inicio < fim){
-                std::cout<<"removido: "<<fila[inicio]<<std::endl;
-                std::cout<<std::endl;
-                inicio++;
-            }else{
-                std::cout<<"fila vazia!"<<std::endl;
-            }
-        }
-        else if(opcao == 3){
-            if(inicio < fim){
-                std::cout<<"fila: ";
-                for(int i = inicio; i < fim; i++){
-                    std::cout<<fila[i]<<" ";
-                }
-                std::cout<<std::endl;
-            }else{
-                std::cout<<"fila vazia!"<<std::endl;
-                std::cout<<std::endl;
-            }
-        }
-        else if(opcao == 4){
-            std::cout<<"saindo..."<<std::endl;
-            break;
         }
     }
-    return 0;
 }
