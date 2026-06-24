7f85fb439055ea9dcc7bc58caa65b74e93bb0b46
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-06-23 21:06:41 -0300

    Buscar número - Array

--- main.cpp
+++ main.cpp
@@ -2,27 +2,37 @@
 
 int main(){
 
-int numeros[] = {1,2,3,4,5,6,7,8,9,10};
-int tamanho = 10;
+    int numeros[] = {10, 20, 30, 40, 50, 60, 70, 80};
+    int tamanho = 8;
+    int buscado;
 
-std::cout<<"===== PARES E IMPARES ====="<<std::endl;
-std::cout<<std::endl;
+    std::cout<<"========= Buscar numero =========="<<std::endl;
+    std::cout<<std::endl;
 
-std::cout<<"Pares: ";
-for(int i = 0; i < tamanho; i++){
-    if(numeros[i] % 2 == 0){
+    std::cout<<"array: ";
+    for(int i = 0; i < tamanho; i++){
         std::cout<<numeros[i]<<" ";
     }
-}
-std::cout<<std::endl;
+    std::cout<<std::endl;
 
-std::cout<<"Impares: ";
-for(int i = 0; i < tamanho; i++){
-    if(numeros[i] % 2 != 0){
-        std::cout<<numeros[i]<<" ";
+    std::cout<<"digite o numero a buscar: ";
+    std::cin>>buscado;
+
+    int posicao = - 1;
+
+    for(int i = 0; i < tamanho; i++){
+        if(numeros[i] == buscado){
+            posicao = i;
+            break;
+        }
     }
-}
 
-
+    std::cout<<std::endl;
+    if(posicao != - 1){
+        std::cout<<"encontrado na posicao "<<posicao<<std::endl;
+    }else{
+        std::cout<<"nao encontrado!"<<std::endl;
+    }
+    
     return 0;
 }
