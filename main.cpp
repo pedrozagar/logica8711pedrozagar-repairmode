8126e4b810ab65a433f71425d6695ed6cdab8a95
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-07-07 20:24:51 -0300

    exercicios em vector - buscar elemento

--- main.cpp
+++ main.cpp
@@ -5,30 +5,34 @@ int main(){
  
     std::vector<int>numeros;
  
-    std::cout<<"==== Contar ocorrencias ===="<<std::endl<<std::endl;
+    std::cout<<"Buscar elemento:"<<std::endl<<std::endl;
    
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(5);
-    numeros.push_back(15);
-    numeros.push_back(15);
-    numeros.push_back(5);
-    numeros.push_back(15);
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300);
+    numeros.push_back(400);
      
-    std::cout<<"vector:"; 
+    std::cout<<"vector: "; 
         for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
+        std::cout<<numeros[i]<<" "; 
     }
 
-    int contador = 0;
-        for(int i = 0; i < numeros.size(); i++){
-            if(numeros[i] == 5){
-                contador ++;
+    std::cout<<"\n";
+
+    int buscado = 300;
+    int posicao = -1; //aqui é -1 porque se fosse 0 seria o números.push_back(100). o 0 faz parte dos numeros lá em cima. o 100 é 0, o 200 é 1, o 300 é 2 e o 400 é 3.
+
+    for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] == buscado){
+            posicao = i;
+            break;
             }
         }
-
-    std::cout<<"\no numero 5 apareceu: "<<contador<<" vezes!"<<std::endl;
+        if(posicao != -1){
+            std::cout<<"\nNumero "<<buscado<<" encontrado na posicao "<<posicao<<std::endl;
+        }else{
+            std::cout<<"numero nao encontrado"<<std::endl;
+        }
  
     return 0;
 }
-
