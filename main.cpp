98ed0a552eddf96b5939684a93dccad735e84688
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-07-07 19:34:11 -0300

    exercicios em vector

--- main.cpp
+++ main.cpp
@@ -1,30 +1,24 @@
 #include<iostream>
 #include<vector>
-
+ 
 int main(){
-
+ 
     std::vector<int>numeros;
-
-    std::cout<<"======= Limpar vector ========"<<std::endl;
-
-    numeros.push_back(100);
-    numeros.push_back(200);
-    numeros.push_back(300);
-    numeros.push_back(400);
-
-    std::cout<<"antes: ";
+ 
+    std::cout<<"==== Soma dos numeros de um Vector ===="<<std::endl;
+   
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
+     
+    int soma = 0;
+ 
     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
+        std::cout<<numeros[i]<<std::endl;
+        soma += numeros[i];
     }
-    std::cout<<std::endl;
-
-    numeros.erase(numeros.begin() + 1);
-
-    std::cout<<"depois: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
-
+    std::cout<<"A soma dos numeros e: "<<soma<<std::endl;
+ 
     return 0;
-
 }
