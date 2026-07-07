95f990eb204cd83cd4e9f77e39e2578861b01e5a
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-07-07 20:50:50 -0300

    adicionar usuario - quantos números quer que apareça?

--- main.cpp
+++ main.cpp
@@ -4,34 +4,24 @@
 int main(){
  
     std::vector<int>numeros;
+    int quantidade;
  
-    std::cout<<"Buscar elemento:"<<std::endl<<std::endl;
+    std::cout<<"adicionar usuario:"<<std::endl<<std::endl;
+
+    std::cout<<"quantos numeros?"<<std::endl;
+    std::cin>>quantidade;
    
-    numeros.push_back(100);
-    numeros.push_back(200);
-    numeros.push_back(300);
-    numeros.push_back(400);
-     
-    std::cout<<"vector: "; 
-        for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" "; 
+    for(int i = 0; i < quantidade; i++){
+        int numero;
+        std::cout<<"digite o numero "<<(i + 1)<<": ";
+        std::cin>>numero;
+        numeros.push_back(numero); 
     }
 
-    std::cout<<"\n";
 
-    int buscado = 300;
-    int posicao = -1; //aqui é -1 porque se fosse 0 seria o números.push_back(100). o 0 faz parte dos numeros lá em cima. o 100 é 0, o 200 é 1, o 300 é 2 e o 400 é 3.
-
-    for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] == buscado){
-            posicao = i;
-            break;
-            }
-        }
-        if(posicao != -1){
-            std::cout<<"\nNumero "<<buscado<<" encontrado na posicao "<<posicao<<std::endl;
-        }else{
-            std::cout<<"numero nao encontrado"<<std::endl;
+    std::cout<<"numeros adicionados: ";
+        for(int i = 0; i < numeros.size(); i++){
+            std::cout<<numeros[i]<<" ";
         }
  
     return 0;
