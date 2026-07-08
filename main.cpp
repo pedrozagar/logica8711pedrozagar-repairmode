203432fcbde02b1f50ca6e26871dc2b09b262aa0
PEDRO ZAGAR <04124949065@senacrs.edu.br>
2026-07-07 22:01:04 -0300

    exercicios vector - numeros pares e impares

--- main.cpp
+++ main.cpp
@@ -4,25 +4,41 @@
 int main(){
  
     std::vector<int>numeros;
-    int quantidade;
- 
-    std::cout<<"adicionar usuario:"<<std::endl<<std::endl;
 
-    std::cout<<"quantos numeros?"<<std::endl;
-    std::cin>>quantidade;
-   
-    for(int i = 0; i < quantidade; i++){
+    numeros.push_back(1);
+    numeros.push_back(2);
+    numeros.push_back(3);
+    numeros.push_back(4);
+    numeros.push_back(5);
+
+    //apresente o vector, mostre os numeros...depois mostre só os pares e depois só os ímpares.
+
+    std::cout<<"numeros pares e impares:""\n\n";
+
+    for(int i = 0; i < numeros.size(); i++){
         int numero;
-        std::cout<<"digite o numero "<<(i + 1)<<": ";
-        std::cin>>numero;
-        numeros.push_back(numero); 
+        std::cout<<numeros[i]<<", ";
+    }
+
+    std::cout<<"\n\n";
+
+    std::cout<<"mostre so os numeros pares: ";
+    for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] % 2 == 0){
+            std::cout<<numeros[i]<<" ";
+        }
+
     }
 
 
-    std::cout<<"numeros adicionados: ";
-        for(int i = 0; i < numeros.size(); i++){
+    std::cout<<"\nmostre so os numeros impar: ";
+    for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] % 2 != 0){
             std::cout<<numeros[i]<<" ";
         }
- 
+
+    }
+
+
     return 0;
 }
